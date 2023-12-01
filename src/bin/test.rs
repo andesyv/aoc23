fe7c96d20d9bcc319e5183552b5deff1fd9de2bd
Anders Syvertsen <andesyv@gmail.com>
2023-12-01 19:47:40 +0100

    Initial commit

---
+++ src/bin/test.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // const INPUT: &str = include_str!("../inputs/1.txt");
+    println!("Hello Advent of Code 2023!");
+}
+
+// #[test]
+// fn example() {
+//     assert_eq!(1, 1)
+// }
